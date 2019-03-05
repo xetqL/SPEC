dd7a8b4859c9ba4e15c827f621072354f8df30b4
Anthony <anthony.boulmier@unige.ch>
2019-03-05 14:49:06 +0100

    lets try

--- src/main.cpp
+++ src/main.cpp
@@ -307,7 +307,8 @@ int main(int argc, char **argv) {
         }
 #elif LB_METHOD==5
         if(i_am_loading_proc) steplogger->info("degradation method 4: ") << ((degradation_since_last_lb*(step-pcall))/2.0) << " avg_lb_cost " << avg_lb_cost;
-        lb_condition = pcall + ncall <= step || ((degradation_since_last_lb*(step-pcall))/2.0 > avg_lb_cost && gossip_waterslope_db.has_converged(7));
+        lb_condition = pcall + ncall <= step || ((degradation_since_last_lb*(step-pcall))/2.0 > avg_lb_cost && gossip_waterslope_db.has_converged(10));
+        std::cout << lb_condition << std::endl;
         if(lb_condition) {
             bool overloading = gossip_waterslope_db.zscore(rank) > 3.0;
             PAR_START_TIMING(current_lb_cost, world);
@@ -315,6 +316,7 @@ int main(int argc, char **argv) {
             PAR_STOP_TIMING(current_lb_cost, world);
             lb_costs.push_back(current_lb_cost);
             gossip_workload_db.reset();
+            gossip_waterslope_db.reset();
             water.clear();
             degradation_since_last_lb = 0.0;
             window_my_time.data_container.clear();
