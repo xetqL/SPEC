756efeb59061365c2879369a328c65ad1d03e4d3
Your Name <you@example.com>
2019-03-28 17:32:09 +0100

    improve QoL

--- src/main.cpp
+++ src/main.cpp
@@ -396,11 +396,10 @@ int main(int argc, char **argv) {
         auto mean  = stats::mean<double>(window_step_time.begin(), window_step_time.end());
 
         if(i_am_foreman) steplogger->info("degradation method 5: ") << degradation_since_last_lb << " avg_lb_cost " << avg_lb_cost << " " << (median-mean);
-        lb_condition = pcall + ncall <= step || degradation_since_last_lb > avg_lb_cost || (pcall == 0 && step > 0 && (median-mean)/mean > 0.05);
+        lb_condition = step==10 || pcall + ncall <= step || degradation_since_last_lb > avg_lb_cost || (pcall == 0 && step > 0 && (median-mean)/mean > 0.05);
         if(lb_condition) {
             bool overloading = gossip_waterslope_db.zscore(rank) > 3.0;
             if(overloading) std::cout << "I WILL BE UNLOADED" << std::endl;
-
             PAR_START_TIMING(current_lb_cost, world);
             stripe_lb.load_balance(&my_cells, overloading ? alpha : 0.0);
             PAR_STOP_TIMING(current_lb_cost, world);
@@ -408,7 +407,6 @@ int main(int argc, char **argv) {
             lb_costs.push_back(current_lb_cost);
             avg_lb_cost = stats::mean<double>(lb_costs.begin(), lb_costs.end());
             perflogger->info("LB_time: ") << current_lb_cost;
-
             gossip_workload_db.reset();
             water.clear();
             degradation_since_last_lb = 0.0;
