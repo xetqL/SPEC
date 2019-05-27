72555d61f1845a1927b2be4c7d43cbb2b39aa4e0
BOULMIER Anthony#865371 <boulmier@login1.cluster>
2019-05-27 14:10:48 +0200

    opt

---
+++ runall_n1_debug.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o xtrace
+partition=$1
+time="0-00:15:00"
+shift
+for P in "$@"
+do
+
+sbatch -p $partition --output %j_N1_ULBA_"$P".out -n $P -c 1 -J SPEC -t $time spec.slurm bin/SPEC_ULBA $P 1 2000 450 12 1 0.3
+
+sbatch -p $partition --output %j_N1_STD_"$P".out -n $P -c 1 -J SPEC -t $time spec.slurm bin/SPEC_STD $P 1 2000 450 12 1 0.3
+
+done 
