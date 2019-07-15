1f11bd375d4c2e0e0655440b4e55f0c0998eb347
xetql <anthony.boulmier@unige.ch>
2019-07-15 12:39:20 +0200

    not debugging anymore <o

--- parse_outfile.py
+++ parse_outfile.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     total_time_std = float([l.split(':')[-1] for l in lines if 'total_time' in l][0])
     print("Total time for the standard approach is:", total_time_std)
     avg_step_time_std = numpy.mean(flatten([map(float, l.split(':')[-1].split(', ')) for l in lines if '"step times"' in l]))
-
+    avg_comp_time_std = numpy.mean(flatten([map(float, l.split(':')[-1].split(', ')) for l in lines if '"comp times"' in l]))
 
     ulba_of  = open(args.ulba, 'r')
     content = ulba_of.read()
@@ -30,8 +30,12 @@ if __name__ == '__main__':
     print("Total time for the ULBA approach is: \t", total_time_ulba)
 
     avg_step_time_ulba = numpy.mean(flatten([map(float, l.split(':')[-1].split(', ')) for l in lines if '"step times"' in l]))
+    avg_comp_time_ulba = numpy.mean(flatten([map(float, l.split(':')[-1].split(', ')) for l in lines if '"comp times"' in l]))
 
     print("\nAverage step time (std)  {0:.3f} s".format(avg_step_time_std))
     print(  "Average step time (ulba) {0:.3f} s".format(avg_step_time_ulba))
+    print("\nAverage comp time (std)  {0:.3f} s".format(avg_comp_time_std))
+    print(  "Average comp time (ulba) {0:.3f} s".format(avg_comp_time_ulba))
+
     print("\nPerformance improvement: {0:.3f} %".format(100.*(total_time_std-total_time_ulba)/total_time_std))
 
