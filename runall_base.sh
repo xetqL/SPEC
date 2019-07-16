23d4413912881d116290e9047568327b5c70401d
xetql <anthony.boulmier@unige.ch>
2019-07-16 09:59:30 +0200

    ok

--- runall_base.sh
+++ runall_base.sh
@@ -9,6 +9,6 @@ do
 
 sbatch --constraint=E5-2630V4 -p $partition --output zoltan_paper_%j_N?_ULBA_"$P".out -n $P -c 1 -J zSPEC_ULBA -t $time spec.slurm bin/SPEC_ULBA $P 1 1000 450 $seed ? 0.2
 
-sbatch --constraint=E5-2630V4 -p $partition --output zoltan_paper_%j_N?_STD_"$P".out -n $P -c 1 -J  zSPEC_STD -t $time spec.slurm bin/SPEC_STD -t $time spec.slurm bin/SPEC_STD $P 1 1000 450 $seed ? 0.2
+sbatch --constraint=E5-2630V4 -p $partition --output zoltan_paper_%j_N?_STD_"$P".out -n $P -c 1 -J  zSPEC_STD -t $time spec.slurm bin/SPEC_STD  $P 1 1000 450 $seed ? 0.2
 
 done 
