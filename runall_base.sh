7f20bbd35c94474233b61d06b1aea1d9a5510efb
xetql <anthony.boulmier@unige.ch>
2019-07-16 09:51:27 +0200

    base script

--- runall_base.sh
+++ runall_base.sh
@@ -7,8 +7,8 @@ seed=$RANDOM
 for P in "$@"
 do
 
-sbatch --constraint=E5-2630V4 -p $partition --output N?_ULBA_"$P"_%j.out -n $P -c 1 -J SPEC_ULBA -t $time spec.slurm bin/SPEC_ULBA $P 1 1000 450 $seed ? 0.2
+sbatch --constraint=E5-2630V4 -p $partition --output zoltan_paper_%j_N?_ULBA_"$P".out -n $P -c 1 -J zSPEC_ULBA -t $time spec.slurm bin/SPEC_ULBA $P 1 1000 450 $seed ? 0.2
 
-sbatch --constraint=E5-2630V4 -p $partition --output N?_STD_"$P"_%j.out -n $P -c 1 -J SPEC_STD -t $time spec.slurm bin/SPEC_STD $P 1 1000 450 $seed ? 0.2
+sbatch --constraint=E5-2630V4 -p $partition --output zoltan_paper_%j_N?_STD_"$P".out -n $P -c 1 -J  zSPEC_STD -t $time spec.slurm bin/SPEC_STD -t $time spec.slurm bin/SPEC_STD $P 1 1000 450 $seed ? 0.2
 
 done 
